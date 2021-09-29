0b4561664f48898a4b57fddcdec37b0dcad2d9eb
chedly99 <mohamed.chedliiy@gmail.com>
2021-09-29 16:28:22 +0100

    fix

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,41 +1,32 @@
 #!/usr/bin/python3
-""" log parsing """
+""" Script that reads stdin line by line and computes metrics """
 
 
-import sys
+def print_dict_sorted_nonzero(status_codes):
+    
 
+    sorted_keys = sorted(status_codes.keys())
+    for k in sorted_keys:
+        if status_codes[k]:
+            print("{:d}: {:d}".format(k, status_codes[k]))
 
-def printx(data, status):
-    """ print the log """
-    print("File size: {}".format(data))
-    for key, value in sorted(status.items()):
-        if value != 0:
-            print("{}: {}".format(key, value))
+if __name__ == "__main__":
+    import sys
 
-
-status = {
-    "200": 0, "301": 0, "400": 0, "401": 0,
-    "403": 0, "404": 0, "405": 0, "500": 0}
-counter = 0
-data = 0
-try:
-    for line in sys.stdin:
-        if counter == 10:
-            printx(data, status)
-            counter = 1
-        else:
-            counter = counter + 1
-        parsed = line.split()
-        try:
-            data = data + int(parsed[-1])
-        except Exception as e:
-            pass
-        try:
-            for key, value in status.items():
-                if key == parsed[-2]:
-                    status[key] = status[key] + 1
-        except Exception as e:
-            pass
-    printx(data, status)
-except KeyboardInterrupt as e:
-    printx(data, status)
+    try:
+        total = 0
+        status_codes = \
+            {code: 0 for code in [200, 301, 400, 401, 403, 404, 405, 500]}
+        for n, line in enumerate(sys.stdin, 1):
+            try:
+                words = line.split()
+                total += int(words[-1])
+                status_codes[int(words[-2])] += 1
+                if n % 10 == 0:
+                    print("File size: {:d}".format(total))
+                    print_dict_sorted_nonzero(status_codes)
+            except ValueError:
+                pass
+    finally:
+        print("File size: {:d}".format(total))
+        print_dict_sorted_nonzero(status_codes)
