ba79b0e9cfce649b83db6ae026434db158dcb946
chedly99 <mohamed.chedliiy@gmail.com>
2022-01-05 16:23:47 +0100

    task0

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+function filmRequest (url) {
+  request(url, async function (error, response, body) {
+    for (const charURL of JSON.parse(body).characters) {
+      if (error) {
+        console.log(`Error: ${error}`);
+        return;
+      }
+      const charJSON = await requestChar(charURL);
+      console.log(JSON.parse(charJSON).name);
+    }
+  });
+}
+
+function requestChar (url) {
+  return new Promise((resolve, reject) => {
+    request(url, function (error, response, body) {
+      if (error) reject(error);
+      else resolve(body);
+    });
+  });
+}
