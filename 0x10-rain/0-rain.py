49b1943f43d12430b9c0fa9c465c9d6393263396
chedly99 <mohamed.chedliiy@gmail.com>
2022-01-06 14:34:52 +0100

    task0

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" 0x10-rain """
+
+
+def rain(walls):
+    """[summary]
+
+    Args:
+        walls ([type]): [description]
+        n ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+
+    if len(walls) == 0:
+        return 0
+    somme = 0
+    for i in range(1, len(walls) - 1):
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
+        right = walls[i]
+
+        for j in range(i + 1, len(walls)):
+            right = max(right, walls[j])
+        somme = somme + (min(left, right) - walls[i])
